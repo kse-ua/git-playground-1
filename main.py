25a06bf30433fd106089de914514de30b0bed174
O-Danylchuk <145114504+O-Danylchuk@users.noreply.github.com>
2023-11-17 04:34:29 +0200

    Additional lose message
    Added additional lose message

--- main.py
+++ main.py
@@ -41,3 +41,4 @@ while not is_game_over():
     else:
         errors += 1
         print(f"Oops :( No such word, you have {ERRORS_TO_LOSE - errors} lives more")
+        print("Sorry, you lose :(")
